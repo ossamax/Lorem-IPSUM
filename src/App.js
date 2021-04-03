b7df445a8aa07d6bf81f35b3591fba1d1ba208c4
ossama zaoute <ramziioussama@gmail.com>
2021-04-03 18:09:31 +0100

    Lorem ipsum generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
 
 function App() {
+  const [num, setNum] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handlSubmit = (e) => {
+    e.preventDefault();
+    let count = parseInt(num);
+
+    if (num <= 0) {
+      count = 1;
+    }
+    if (num > 8) {
+      count = 8;
+    }
+    setText(data.slice(0, count));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>Lorem IPSUM Generator</header>
+        <form onSubmit={handlSubmit}>
+          <label htmlFor="number">Paragraphs</label>
+          <input
+            type="number"
+            name="number"
+            value={num}
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
+          <button type="submit">Generate</button>
+        </form>
+        <article>
+          {text.map((p, i) => {
+            return <p key={i}>{p}</p>;
+          })}
+        </article>
+      </div>
     </div>
   );
 }
